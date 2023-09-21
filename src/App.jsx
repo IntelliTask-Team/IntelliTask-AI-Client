fe2d38e93eb663ccfa9c502d8631969af3f2c14d
Alexis Parsat-Lacoste <alexis@parsat-lacoste.com>
2023-09-21 17:10:46 +0200

    Add Routes for Navbar and HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-// import Navbar from "./components/Navbar";
-// import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
 import ProjectDetailsPage from "./pages/ProjectDetailsPage";
 
 function App() {
-
   return (
     <div>
-      {/* <Navbar /> */}
+      <Navbar />
 
       <Routes>
-        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/" element={<HomePage />} />
         <Route path="/projects/:projectId" element={<ProjectDetailsPage />} />
       </Routes>
     </div>
