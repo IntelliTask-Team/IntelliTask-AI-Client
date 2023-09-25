38975ff1ddb2291f3eef84e4ce3a076e52a4eb88
Kim Idsinga <kimidsinga@gmail.com>
2023-09-25 17:33:38 +0200

    add local storage JWT token

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
 
 const API_URL = "http://localhost:5005";
 
@@ -8,9 +9,12 @@ function LoginPage(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState(undefined);
-
+  
   const navigate = useNavigate();
 
+  /*  UPDATE - get authenticateUser from the context */
+  const { storeToken, authenticateUser } = useContext(AuthContext);
+  
   const handleEmail = (e) => setEmail(e.target.value);
   const handlePassword = (e) => setPassword(e.target.value);
 
@@ -18,28 +22,36 @@ function LoginPage(props) {
     e.preventDefault();
     const requestBody = { email, password };
 
-    axios
-      .post(`${API_URL}/auth/login`, requestBody)
+    axios.post(`${API_URL}/auth/login`, requestBody)
       .then((response) => {
-        // Request to the server's endpoint `/auth/login` returns a response
-        // with the JWT string ->  response.data.authToken
-        console.log("JWT token", response.data.authToken);
-
-        navigate("/"); 
+        console.log('JWT token', response.data.authToken );
+      
+        // Save the token in the localStorage.      
+        storeToken(response.data.authToken);
+        
+        // Verify the token by sending a request 
+        // to the server's JWT validation endpoint. 
+        authenticateUser();     
+        navigate('/');
       })
       .catch((error) => {
         const errorDescription = error.response.data.message;
         setErrorMessage(errorDescription);
-      });
+      })
   };
-
+  
   return (
     <div className="LoginPage">
       <h1>Login</h1>
 
       <form onSubmit={handleLoginSubmit}>
         <label>Email:</label>
-        <input type="email" name="email" value={email} onChange={handleEmail} />
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleEmail}
+        />
 
         <label>Password:</label>
         <input
@@ -51,12 +63,12 @@ function LoginPage(props) {
 
         <button type="submit">Login</button>
       </form>
-      {errorMessage && <p className="error-message">{errorMessage}</p>}
+      { errorMessage && <p className="error-message">{errorMessage}</p> }
 
       <p>Don't have an account yet?</p>
       <Link to={"/signup"}> Sign Up</Link>
     </div>
-  );
+  )
 }
 
 export default LoginPage;
