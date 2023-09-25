e885c3f3d6656ba8f2bf9c20fed3876b4c134a7e
Alexis Parsat-Lacoste <alexis@parsat-lacoste.com>
2023-09-25 14:59:57 +0200

    Implement Open AI API (frontend)

--- src/components/Ai.jsx
+++ src/components/Ai.jsx
@@ -1,11 +1,34 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const API_URL = "http://localhost:5005";
+
 function Ai() {
-    return (
-        <div>
-        <h2>AI Magic</h2>
-        <p>With the response</p>
-      </div>
-    );
-  }
-  
-  export default Ai;
-  
+  const [response, setResponse] = useState(null);
+  const [apiCalled, setApiCalled] = useState(false);
+
+  const handleApiCall = () => {
+    if (!apiCalled) {
+      const prompt = "Tell me a super short joke about coding";
+      axios
+        .post(`${API_URL}/api/openai`, { prompt })
+        .then((apiResponse) => {
+          setResponse(apiResponse.data.choices[0].message.content);
+          setApiCalled(true);
+        })
+        .catch((error) => {
+          console.error("OpenAI Error : ", error);
+        });
+    }
+  };
+
+  return (
+    <div>
+      <h2>Ask our AI for help</h2>
+      {response && <p>Response from AI: {response}</p>}
+      <button onClick={handleApiCall}>Ask the AI</button>
+    </div>
+  );
+}
+
+export default Ai;
