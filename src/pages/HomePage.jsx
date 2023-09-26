e83d7848ab2f4a90f7b510a7390fe3a0129bab8c
Alexis Parsat-Lacoste <alexis@parsat-lacoste.com>
2023-09-26 09:50:03 +0200

    Tweak CSS on navbar and homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -22,7 +22,7 @@ function HomePage() {
 
   return (
     <div className="HomePage relative pb-20 px-5 md:px-10 w-full 2xl:w-1/2 max-w-4xl">
-      <h1 className="mt-20 mb-5 text-3xl font-bold tracking-tight text-gray-900 text-center text-sapin">
+      <h1 className="mt-20 mb-5 text-3xl font-bold tracking-tight text-gray-900 text-center">
         AI Powered Projects
       </h1>
       <Link to={`/`}>
