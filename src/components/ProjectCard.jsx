6a8612f593c6b39c3af4fbf3d4c7376cae6d68a4
Alexis Parsat-Lacoste <alexis@parsat-lacoste.com>
2023-09-26 22:43:07 +0200

    Fix responsive on ProjectCard

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function ProjectCard({ title, description, _id, tasks, ai }) {
   return (
     <Link to={`/projects/${_id}`}>
-      <div className="bg-white p-5 my-5 rounded-xl mx-auto shadow flex flex-col md:flex-row justify-between items-center">
+      <div className="bg-white p-5 my-5 rounded-xl mx-auto shadow flex flex-col md:flex-row justify-between items-start md:items-center">
         <div>
           <h2 className="text-xl font-medium pb-2">{title}</h2>
           <p className="pr-3">{description} </p>
