df9e7a40494dc1455c052f55ca9acea84de77646
Alexis Parsat-Lacoste <alexis@parsat-lacoste.com>
2023-09-26 16:47:22 +0200

    Implement LoginPage CSS and responsive

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -40,27 +40,60 @@ function LoginPage(props) {
   };
 
   return (
-    <div className="LoginPage">
-      <h1>Login</h1>
+    <div className="LoginPage w-full bg-white rounded-lg shadow sm:max-w-md md:mt-28">
+      <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+          Welcome back !
+        </h1>
 
-      <form onSubmit={handleLoginSubmit}>
-        <label>Email:</label>
-        <input type="email" name="email" value={email} onChange={handleEmail} />
+        <form onSubmit={handleLoginSubmit}>
+          <div className="pb-2">
+            <label>Email</label>
+          </div>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleEmail}
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 mb-5"
+            placeholder="yourname@mail.com"
+          />
 
-        <label>Password:</label>
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={handlePassword}
-        />
+          <div className="pb-2">
+            <label>Password</label>
+          </div>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handlePassword}
+            placeholder="• • • • • • • •"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 mb-8"
+          />
 
-        <button type="submit">Login</button>
-      </form>
-      {errorMessage && <p className="error-message">{errorMessage}</p>}
+          <button
+            type="submit"
+            className="w-full text-white bg-vert hover:bg-emerald-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+          >
+            Log In
+          </button>
+        </form>
+        {errorMessage && (
+          <p className="error-message text-sm font-light text-gray-500">
+            {errorMessage}
+          </p>
+        )}
 
-      <p>Don't have an account yet?</p>
-      <Link to={"/signup"}> Sign Up</Link>
+        <p className="text-sm font-light text-gray-500">
+          Don't have an account yet ?
+          <a
+            href="/signup"
+            className="font-medium text-primary-600 hover:underline pl-2"
+          >
+            Register
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
