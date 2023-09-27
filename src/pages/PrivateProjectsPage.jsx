79c75d24bd9c384dc8d3f81e6bf8a8e618af4036
Kim Idsinga <kimidsinga@gmail.com>
2023-09-27 17:17:55 +0200

    added private pages for logged in users

---
+++ src/pages/PrivateProjectsPage.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+import ProjectCard from "../components/ProjectCard";
+// import { AuthContext } from "../context/auth.context";
+
+function PrivateProjectsPage() {
+  const storedToken = localStorage.getItem("authToken");
+  const [projects, setProjects] = useState([]);
+ 
+  const getAllProjects = () => {
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/api/projects`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then((response) => setProjects(response.data))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+   
+    getAllProjects();
+  }, []);
+
+
+  return (
+    <div className="HomePage relative pb-20 px-5 md:px-10 w-full 2xl:w-1/2 max-w-4xl">
+      <h1 className="mt-20 mb-5 text-3xl font-bold tracking-tight text-gray-900 text-center">
+        AI Powered Projects
+      </h1>
+      <Link to={`/`}>
+        <p className="mb-20 text-xs font-bold tracking-tight text-gray-400 text-center">
+          HOW DOES IT WORKS?
+        </p>
+      </Link>
+
+      {projects.map((project) => (
+        <ProjectCard key={project._id} {...project} />
+      ))}
+    </div>
+  );
+}
+
+export default PrivateProjectsPage;
