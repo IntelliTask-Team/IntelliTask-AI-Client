a28a8274e0e0781b8dc237ae0c3f2c430ef92c82
Kim Idsinga <kimidsinga@gmail.com>
2023-09-27 17:50:29 +0200

    remove comment and error from HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,34 +5,6 @@ import { Link } from "react-router-dom";
 import ProjectCard from "../components/ProjectCard";
 
 function HomePage() {
-
-  // const [projects, setProjects] = useState([]);
-
-  // const getAllProjects = () => {
-  //   axios
-  //     .get(`${import.meta.env.VITE_API_URL}/api/projects`)
-  //     .then((response) => setProjects(response.data))
-  //     .catch((error) => console.log(error));
-  // };
-
-//   const [projects, setProjects] = useState([]);
-//   const [isLoading, setIsLoading] = useState(true);
-
-//   const getAllProjects = () => {
-//     axios
-//       .get(`${import.meta.env.VITE_API_URL}/api/projects`)
-//       .then((response) => {
-//         setProjects(response.data);
-//         setIsLoading(false);
-//       })
-//       .catch((error) => console.log(error));
-//   };
-
-
-  // useEffect(() => {
-  //   getAllProjects();
-  // }, []);
-
   return (
     <div className="HomePage relative pb-20 px-5 md:px-10 w-full 2xl:w-1/2 max-w-4xl mb-10">
       <h1 className="mt-20 mb-5 text-3xl font-bold tracking-tight text-gray-900 text-center">
@@ -43,24 +15,7 @@ function HomePage() {
           CREATE A PROJECT
         </p>
       </Link>
-
-
       <h2>PROJECTS WILL BE ADDED LATER</h2>
-      {/* THIS WILL BE ADDED LATER */}
-      {/* {projects.map((project) => (
-        <ProjectCard key={project._id} {...project} />
-      ))} */}
-
-//       {isLoading ? (
-//         <div className="flex flex-col justify-start mx-auto w-40">
-//           <img src="./images/waiting.gif" alt="Loading GIF" />
-//         </div>
-//       ) : (
-//         projects.map((project) => (
-//           <ProjectCard key={project._id} {...project} />
-//         ))
-//       )}
-
     </div>
   );
 }
