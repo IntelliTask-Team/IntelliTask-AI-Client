4cfe8aaeca3ff9c8cc220d2b61037ceb149214cf
Kim Idsinga <kimidsinga@gmail.com>
2023-09-27 22:07:12 +0200

    Added demo homepage and projectdetails

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,36 +1,49 @@
 import axios from "axios";
+import { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
 
 function TaskCard({ task, tasks, updateTasks, provided, innerRef }) {
+  const { isLoggedIn } = useContext(AuthContext);
   const storedToken = localStorage.getItem("authToken");
   const { description, _id } = task;
 
   // ***** DELETE TASK *****
   const handleDelete = () => {
-    axios
-      .delete(`${import.meta.env.VITE_API_URL}/api/tasks/${_id}`, {
-        headers: { Authorization: `Bearer ${storedToken}` },
-      })
-      .then(() => {
-        console.log("Task deleted from backend. Now updating frontend.");
-        // Filter out the deleted task from the current task list
-        const updatedTasks = tasks.filter((task) => task._id !== _id);
-        console.log("Updated tasks after deletion:", updatedTasks);
-        updateTasks(updatedTasks); // Update parent's task state with this new list
-      })
-      .catch((error) => console.log(error));
+    if (isLoggedIn) {
+      axios
+        .delete(`${import.meta.env.VITE_API_URL}/api/tasks/${_id}`, {
+          headers: { Authorization: `Bearer ${storedToken}` },
+        })
+        .then(() => {
+          console.log("Task deleted from backend. Now updating frontend.");
+          updateTasksFrontend();
+        })
+        .catch((error) => console.log(error));
+    } else {
+      updateTasksFrontend();
+    }
+  };
+
+  const updateTasksFrontend = () => {
+    const updatedTasks = tasks.filter((task) => task._id !== _id);
+    updateTasks(updatedTasks);
   };
 
   return (
-    <div ref={innerRef} {...provided.draggableProps}>
-      <div className="flex flex-row mb-2">
+    <div
+      className="flex flex-row items-center mb-2 border-b pb-2"
+      ref={innerRef}
+      {...provided.draggableProps}
+    >
+      <div className="flex flex-row items-center flex-grow">
         <span {...provided.dragHandleProps}>
           <img src="/images/drag.png" className="w-5 mr-2 cursor-pointer" />
         </span>
         <div>{description}</div>
-        <span onClick={handleDelete}>
-          <img src="/images/bin.png" className="w-5 ml-2 cursor-pointer" />
-        </span>
       </div>
+      <span onClick={handleDelete}>
+        <img src="/images/bin.png" className="w-5 ml-2 cursor-pointer" />
+      </span>
     </div>
   );
 }
