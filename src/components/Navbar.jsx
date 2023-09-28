37ffe69def0708384d31177d867c1cd09e9ec062
Alexis Parsat-Lacoste <alexis@parsat-lacoste.com>
2023-09-28 17:36:38 +0200

    Design the landing page with responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,57 +7,57 @@ function Navbar() {
 
   return (
     <nav className="flex items-center justify-center lg:justify-between flex-wrap bg-jaune p-6 md:px-20 w-full">
-
-       {/* NOT LOGGED IN START PAGE = HOME PAGE WITH DEMO PROJECTS */}
+      {/* NOT LOGGED IN START PAGE = HOME PAGE WITH DEMO PROJECTS */}
       {!isLoggedIn && (
-            <>
-      <Link to={`/`}>
-        <div className="flex items-center flex-shrink-0 text-white mr-6">
-          <img
-            className="fill-current h-8 w-8 mr-3 mb-1"
-            width="54"
-            height="54"
-            viewBox="0 0 54 54"
-            src="/images/intellitask-ai.png"
-            />
-          <p className="font-bold text-2xl text-white">
-            IntelliTask <span className="font-light text-xl">AI</span>
-          </p>
-        </div>
-      </Link>
-            </>)}
+        <>
+          <Link to={`/`}>
+            <div className="flex items-center flex-shrink-0 text-white mr-6">
+              <img
+                className="fill-current h-8 w-8 mr-3 mb-1"
+                width="54"
+                height="54"
+                viewBox="0 0 54 54"
+                src="/images/intellitask-ai.png"
+              />
+              <p className="font-bold text-2xl text-white">
+                IntelliTask <span className="font-light text-xl">AI</span>
+              </p>
+            </div>
+          </Link>
+        </>
+      )}
 
- {/* LOGGED IN START PAGE = PROJECTS PAGE*/}
+      {/* LOGGED IN START PAGE = PROJECTS PAGE*/}
       {isLoggedIn && (
-            <>
-      <Link to={`/projects`}>
-        <div className="flex items-center flex-shrink-0 text-white mr-6">
-          <img
-            className="fill-current h-8 w-8 mr-3 mb-1"
-            width="54"
-            height="54"
-            viewBox="0 0 54 54"
-            src="/images/intellitask-ai.png"
-            />
-          <p className="font-bold text-2xl text-white">
-            IntelliTask <span className="font-light text-xl">AI</span>
-          </p>
-        </div>
-      </Link>
-            </>)}
+        <>
+          <Link to={`/projects`}>
+            <div className="flex items-center flex-shrink-0 text-white mr-6">
+              <img
+                className="fill-current h-8 w-8 mr-3 mb-1"
+                width="54"
+                height="54"
+                viewBox="0 0 54 54"
+                src="/images/intellitask-ai.png"
+              />
+              <p className="font-bold text-2xl text-white">
+                IntelliTask <span className="font-light text-xl">AI</span>
+              </p>
+            </div>
+          </Link>
+        </>
+      )}
 
       <div className="w-full block flex-grow flex flex-col items-center lg:flex-row lg:items-center lg:w-auto">
         <div className="text-sm lg:flex-grow flex flex-row justify-end lg:mr-7">
-
           {/* LOGGED IN SHOW PROJECTS & LOGOUT */}
           {isLoggedIn && (
             <>
-          <Link
-            to={`/projects`}
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
-          >
-            My Projects
-          </Link>
+              <Link
+                to={`/projects`}
+                className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
+              >
+                My Projects
+              </Link>
 
               <Link
                 to={`/`}
@@ -71,12 +71,12 @@ function Navbar() {
           {/* NOT LOGGED IN SHOW PROJECTS, LOGIN & REGISTER */}
           {!isLoggedIn && (
             <>
-                      <Link
-            to={`/`}
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
-          >
-            Demo Projects
-          </Link>
+              <Link
+                to={`/`}
+                className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
+              >
+                Demo Projects
+              </Link>
               <Link
                 to={`/login`}
                 className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
@@ -101,7 +101,7 @@ function Navbar() {
                 to={`/create-project`}
                 className="inline-block text-sm px-4 py-2 leading-none font-medium border rounded bg-vert text-white border-transparent hover:bg-emerald-700 mt-4 lg:mt-0"
               >
-                New AI Project
+                New Project
               </Link>
             </>
           )}
@@ -113,7 +113,7 @@ function Navbar() {
                 to={`/login`}
                 className="inline-block text-sm px-4 py-2 leading-none font-medium border rounded bg-vert text-white border-transparent hover:bg-emerald-700 mt-4 lg:mt-0"
               >
-                New AI Project
+                New Project
               </Link>
             </>
           )}
