6edbe40b5affe35026ef401e5d75acfdd3904882
Kim Idsinga <kimidsinga@gmail.com>
2023-09-28 19:33:28 +0200

    Fix positions of loading gif on the pages

--- src/pages/PrivateProjectDetailsPage.jsx
+++ src/pages/PrivateProjectDetailsPage.jsx
@@ -27,7 +27,7 @@ function PrivateProjectDetailsPage() {
       })
       .catch((err) => {
         console.log(err);
-        setIsLoading(false);
+        setIsLoading(false); 
       });
   };
 
@@ -83,84 +83,82 @@ function PrivateProjectDetailsPage() {
 
   return (
     <>
+      {isLoading ? (
+        <div className="flex flex-col justify-center m-auto w-20">
+          <img src="/images/waiting.gif" alt="Loading GIF" />
+        </div>
+      ) : ( 
       <div className="w-full lg:w-1/2 bg-white rounded-lg shadow sm:max-w-md md:max-w-xl lg:max-w-2xl md:mt-28 mb-40">
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-          {isLoading ? (
-            <div className="flex flex-col justify-center m-auto w-20">
-              <img src="/images/waiting.gif" alt="Loading GIF" />
-            </div>
-          ) : (
-            <>
-              {/* ***** DISPLAY PROJECT DETAILS ***** */}
-              {project && (
-                <div>
-                  {isEditing ? (
-                    <ProjectEdit project={project} onSave={handleEditSave} />
-                  ) : (
-                    <>
-                      <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl mb-2">
-                        {project.title}
-                      </h1>
-                      <p className="font-light mb-3">{project.description}</p>
-                      <button
-                        className="inline-block max-h-8 text-sm px-4 py-2 leading-none font-medium border rounded bg-stone-300 text-stone-500 hover:text-stone-800 mt-4 lg:mt-0 mb-4"
-                        onClick={() => setIsEditing(true)}
-                      >
-                        Edit
-                      </button>
-                    </>
-                  )}
-                </div>
-              )}
-
-              {/* ***** DISPLAY TASKS WITH DRAG AND DROP ***** */}
-
-              <h2 className="text-xl font-semibold leading-tight tracking-tight text-gray-900 md:text-xl">
-                Your tasks for this project
-              </h2>
-              {project && project.tasks && project.tasks.length > 0 ? (
-                <TaskListDnD
-                  tasks={project.tasks}
-                  updateTasks={handleTasksUpdate}
-                />
+          {/* ***** DISPLAY PROJECT DETAILS ***** */}
+          {project && (
+            <div>
+              {isEditing ? (
+                <ProjectEdit project={project} onSave={handleEditSave} />
               ) : (
-                <p className="text-sm font-light text-gray-500 mt-0">
-                  You don't have any tasks yet, get inspired by clicking "Ask
-                  the AI" below.
-                </p>
+                <>
+                  <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl mb-2">
+                    {project.title}
+                  </h1>
+                  <p className="font-light mb-3">{project.description}</p>
+                  <button
+                    className="inline-block max-h-8 text-sm px-4 py-2 leading-none font-medium border rounded bg-stone-300 text-stone-500 hover:text-stone-800 mt-4 lg:mt-0 mb-4"
+                    onClick={() => setIsEditing(true)}
+                  >
+                    Edit
+                  </button>
+                </>
               )}
-
-              {/* ***** CREATE NEW TASK ***** */}
-              {project && (
-                <TaskCreate
-                  projectId={projectId}
-                  updateTasks={getProject}
-                  currentOrder={
-                    project.tasks && project.tasks.length > 0
-                      ? project.tasks[project.tasks.length - 1].order + 1
-                      : 0
-                  }
-                />
-              )}
-
-              {/* ***** DISPLAY AI RESPONSE ***** */}
-              {project && (
-                <Ai
-                  projectDescription={project.description}
-                  projectTitle={project.title}
-                  projectTasks={project.tasks}
-                />
-              )}
-
-              {/* ***** DELETE THE PROJECT & ATTACHED TASKS BUTTON ***** */}
-              <ProjectDelete
-                projectId={projectId}
-                completeDeleteSuccess={completeDeleteSuccess}
-              />
-            </>
+            </div>
           )}
+
+          {/* ***** DISPLAY TASKS WITH DRAG AND DROP ***** */}
+
+          <h2 className="text-xl font-semibold leading-tight tracking-tight text-gray-900 md:text-xl">
+            Your tasks for this project
+          </h2>
+          {project && project.tasks && project.tasks.length > 0 ? (
+            <TaskListDnD
+              tasks={project.tasks}
+              updateTasks={handleTasksUpdate}
+            />
+          ) : (
+            <p className="text-sm font-light text-gray-500 mt-0">
+              You don't have any tasks yet, get inspired by clicking "Ask the
+              AI" below.
+            </p>
+          )}
+
+          {/* ***** CREATE NEW TASK ***** */}
+          {project && (
+            <TaskCreate
+              projectId={projectId}
+              updateTasks={getProject}
+              currentOrder={
+                project.tasks && project.tasks.length > 0
+                  ? project.tasks[project.tasks.length - 1].order + 1
+                  : 0
+              }
+            />
+          )}
+
+          {/* ***** DISPLAY AI RESPONSE ***** */}
+          {project && (
+            <Ai
+              projectDescription={project.description}
+              projectTitle={project.title}
+              projectTasks={project.tasks}
+            />
+          )}
+
+          {/* ***** DELETE THE PROJECT & ATTACHED TASKS BUTTON ***** */}
+          <ProjectDelete
+            projectId={projectId}
+            completeDeleteSuccess={completeDeleteSuccess}
+          />
         </div>
       </div>
+      )}
     </>
   );
 }
