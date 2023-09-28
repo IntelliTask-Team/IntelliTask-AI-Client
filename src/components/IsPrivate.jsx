32c5c7a11317cae33b01b792de3d3cb8bd24dee7
Kim Idsinga <kimidsinga@gmail.com>
2023-09-28 18:01:47 +0200

    Adding loading animations if  page is loading

--- src/components/IsPrivate.jsx
+++ src/components/IsPrivate.jsx
@@ -3,17 +3,24 @@ import { AuthContext } from "../context/auth.context";
 import { Navigate } from "react-router-dom";
 
 function IsPrivate( { children } ) {
-  
   const { isLoggedIn, isLoading } = useContext(AuthContext);
 
-  // If the authentication is still loading 
-  if (isLoading) return <p>Loading ...</p>;
+  // If the authentication is still loading
+  if (isLoading) {
+    return (
+      <>
+        <div className="flex flex-col justify-center m-auto w-20">
+          <img src="/images/waiting.gif" alt="Loading GIF" />
+        </div>
+      </>
+    );
+  }
 
   if (!isLoggedIn) {
-  // If the user is not logged in 
+    // If the user is not logged in
     return <Navigate to="/login" />;
   } else {
-  // If the user is logged in, allow to see the page 
+    // If the user is logged in, allow to see the page
     return children;
   }
 }
