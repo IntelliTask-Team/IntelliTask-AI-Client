82f5e349f75acd7399fb84e00515695d1e92cd31
Kim Idsinga <kimidsinga@gmail.com>
2023-09-28 15:36:09 +0200

    added checkbox functionality for tasks

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -28,6 +28,27 @@ function TaskCard({ task, tasks, updateTasks, provided, innerRef }) {
     updateTasks(updatedTasks);
   };
 
+    // ***** CHECKBOX TASK *****
+    const handleCheckboxChange = () => {
+      if (isLoggedIn) {
+        axios.put(`${import.meta.env.VITE_API_URL}/api/tasks/${_id}`, {
+          completed: !task.completed
+        }, {
+          headers: { Authorization: `Bearer ${storedToken}` }
+        })
+        .then(response => {
+          const updatedTask = response.data;
+          const updatedTasks = tasks.map(task => task._id === updatedTask._id ? updatedTask : task);
+          updateTasks(updatedTasks);
+        })
+        .catch(error => console.log(error));
+      } else {
+        const demoUpdatedTask = { ...task, completed: !task.completed };
+        const updatedTasks = tasks.map(task => task._id === demoUpdatedTask._id ? demoUpdatedTask : task);
+        updateTasks(updatedTasks);
+      }
+    };
+
   return (
     <div
       className="flex flex-row items-center mb-2 border-b pb-2"
@@ -38,7 +59,12 @@ function TaskCard({ task, tasks, updateTasks, provided, innerRef }) {
         <span {...provided.dragHandleProps}>
           <img src="/images/drag.png" className="w-5 mr-2 cursor-pointer" />
         </span>
-        <div>{description}</div>
+        <input 
+        type="checkbox" 
+        checked={task.completed} 
+        onChange={handleCheckboxChange} 
+      />
+      <div style={{textDecoration: task.completed ? 'line-through' : 'none'}}>{description}</div>
       </div>
       <span onClick={handleDelete}>
         <img src="/images/bin.png" className="w-5 ml-2 cursor-pointer" />
