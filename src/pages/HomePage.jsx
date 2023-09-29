ffe5e5464550d68cfbcc33face8aa1d6593d6b71
Alexis Parsat-Lacoste <alexis@parsat-lacoste.com>
2023-09-29 10:20:59 +0200

    Add HomePage copywriting

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -27,7 +27,7 @@ function PrivateProjectsPage() {
     <div className="HomePage relative pb-20 px-5 md:px-10 w-full 2xl:w-3/4 max-w-6xl mb-20">
       <div className="flex flex-col items-center">
         <h1 className="mt-20 mb-5 text-3xl font-bold tracking-tight text-gray-900 text-center">
-          AI Powered Projects
+          Projects Enhanced by AI
         </h1>
         <Link to={`/signup`}>
           <p className="mb-20 text-xs font-bold tracking-tight text-gray-400 text-center">
@@ -45,15 +45,14 @@ function PrivateProjectsPage() {
           </div>
           <div className="w-full md:w-1/2 flex flex-col items-start justify-center ">
             <h2 className="text-xl font-medium pb-2">
-              Lorem ipsum sit dolor amet
+              Get a task breakdown of your projects
             </h2>
             <p className="mb-4 font-light">
-              Lorem ipsum sit dolor amet. Lorem ipsum sit dolor amet. Lorem
-              ipsum sit dolor amet. Lorem ipsum sit dolor amet. Lorem ipsum sit
-              dolor amet. Lorem ipsum sit dolor amet. Lorem ipsum sit dolor
-              amet. Lorem ipsum sit dolor amet. Lorem ipsum sit dolor amet.
-              Lorem ipsum sit dolor amet. Lorem ipsum sit dolor amet. Lorem
-              ipsum sit dolor amet.
+              Effortlessly obtain task breakdowns for your projects with our
+              user-friendly platform. Harness the power of AI to simplify
+              project planning and tracking. Create, organize, and monitor your
+              tasks with ease, all at no cost. Welcome to project management
+              made simple.
             </p>
             <Link to={`/signup`}>
               <div className="inline-block text-sm px-4 py-3 leading-none font-medium border rounded bg-vert text-white border-transparent hover:bg-emerald-700 mt-4 lg:mt-0">
@@ -65,43 +64,35 @@ function PrivateProjectsPage() {
 
         <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 pb-20">
           <div className="flex flex-col justify-center items-center w-full text-center">
-            <img
-              src="./images/illus.png"
-              alt="Illustration of a task manager"
-              className="pb-6 w-32"
-            />
-            <h3 className="text-md font-medium pb-2">
-              Lorem ipsum sit dolor amet sin.
+            <div className="mb-6 w-20 h-20 bg-jaune rounded-full flex justify-center items-center border-4 border-white">
+              <p className="text-white font-semibold text-3xl">1</p>
+            </div>
+            <h3 className="text-md font-regular pb-2">
+              Initiate projects on your dedicated page.
             </h3>
           </div>
           <div className="flex flex-col justify-center items-center w-full text-center">
-            <img
-              src="./images/illus.png"
-              alt="Illustration of a task manager"
-              className="pb-6 w-32"
-            />
-            <h3 className="text-md font-medium pb-2">
-              Lorem ipsum sit dolor amet sin.
+            <div className="mb-6 w-20 h-20 bg-jaune rounded-full flex justify-center items-center border-4 border-white">
+              <p className="text-white font-semibold text-3xl">2</p>
+            </div>
+            <h3 className="text-md font-regular pb-2">
+              Provide a description for the AI to grasp your goals.
             </h3>
           </div>
           <div className="flex flex-col justify-center items-center w-full text-center">
-            <img
-              src="./images/illus.png"
-              alt="Illustration of a task manager"
-              className="pb-6 w-32"
-            />
-            <h3 className="text-md font-medium pb-2">
-              Lorem ipsum sit dolor amet sin.
+            <div className="mb-6 w-20 h-20 bg-jaune rounded-full flex justify-center items-center border-4 border-white">
+              <p className="text-white font-semibold text-3xl">3</p>
+            </div>
+            <h3 className="text-md font-regular pb-2">
+              Let the AI suggest you a task breakdown in seconds.
             </h3>
           </div>
           <div className="flex flex-col justify-center items-center w-full text-center">
-            <img
-              src="./images/illus.png"
-              alt="Illustration of a task manager"
-              className="pb-6 w-32"
-            />
-            <h3 className="text-md font-medium pb-2">
-              Lorem ipsum sit dolor amet sin.
+            <div className="mb-6 w-20 h-20 bg-jaune rounded-full flex justify-center items-center border-4 border-white">
+              <p className="text-white font-semibold text-3xl">4</p>
+            </div>
+            <h3 className="text-md font-regular pb-2">
+              Write tasks and keep track of your progress.
             </h3>
           </div>
         </section>
